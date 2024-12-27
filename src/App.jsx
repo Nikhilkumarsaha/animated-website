40802fd5e9640c25fc0f3f92a2a88c49b780bbe8
ashishbehera29 <87660082+ashishbehera29@users.noreply.github.com>
2024-12-27 18:20:49 +0530

    try

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 
 const App = () => {
   return (
     <main>
       <h1 className="text-5xl text-violet-300 font-bold">Welcome to Tailwind</h1>
-      <h1>Hello commit</h1>
+      
     </main>
   )
 }
