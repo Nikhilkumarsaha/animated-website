af21e2ca3cfefd832599234f0882d949b739af66
Satyam-Nayak <satyamn120@gmail.com>
2024-12-27 19:01:40 +0530

    About.jsx 1 Animated added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,15 @@
-
+import React from 'react'
+import AnimatedTitle from './AnimatedTitle'
 const About = () => {
   return (
-    <div>
-      <h1>about</h1>
+    <div id="about" className='min-h-screen w-scree'>
+      <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+        <h2 className="font-general text-sm uppercase md:text-[10px]">Welcoome to BTM</h2>
+        <AnimatedTitle/>
+      </div>
+
     </div>
   )
 }
-
 export default About
 
