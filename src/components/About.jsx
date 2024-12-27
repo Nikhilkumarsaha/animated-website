e5573a537baed9e032ae1ea828ec81953c6f2e9a
Satyam-Nayak <satyamn120@gmail.com>
2024-12-27 19:15:33 +0530

    add bold txxt to the about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <AnimatedTitle/>
 
         <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
-          Discover the world's largest scam city 'Bangalore'
+          Disc<b>o</b>ver the world's <br /> l<b>a</b>rgest scam city 'Ban<b>gal</b>ore'
         </div>
       </div>
     </div>
