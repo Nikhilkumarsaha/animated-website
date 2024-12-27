a879c726348425f5867be3ef4f8c2d72fdd214c1
Nikhil kumar saha <nikhilsaha92236@gmail.com>
2024-12-28 02:31:21 +0530

    Story and animation

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,5 +1,9 @@
-import React, { useEffect, useRef } from 'react';
-import gsap from "gsap";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import clsx from "clsx";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const AnimatedTitle = ({ title, containerClass }) => {
   const containerRef = useRef(null);
@@ -12,7 +16,6 @@ const AnimatedTitle = ({ title, containerClass }) => {
           start: "100 bottom",
           end: "center bottom",
           toggleActions: "play none none reverse",
-          
         },
       });
 
@@ -23,19 +26,27 @@ const AnimatedTitle = ({ title, containerClass }) => {
           transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
           ease: "power2.inOut",
           stagger: 0.02,
-          color: "black",
         },
         0
       );
     }, containerRef);
+
+    return () => ctx.revert(); // Clean up on unmount
   }, []);
 
   return (
-    <div ref={containerRef} className={`animated-title ${containerClass}`}>
-      {title.split('<br />').map((line, index) => (
-        <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
-          {line.split(' ').map((word, i) => (
-            <span key={i} className="animated-word" dangerouslySetInnerHTML={{ __html: word }} />
+    <div ref={containerRef} className={clsx("animated-title", containerClass)}>
+      {title.split("<br />").map((line, index) => (
+        <div
+          key={index}
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+        >
+          {line.split(" ").map((word, idx) => (
+            <span
+              key={idx}
+              className="animated-word"
+              dangerouslySetInnerHTML={{ __html: word }}
+            />
           ))}
         </div>
       ))}
