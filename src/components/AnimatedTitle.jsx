4130db285b0160753fe85ba07e7a0c24525005cb
Nikhil kumar saha <nikhilsaha92236@gmail.com>
2024-12-27 19:14:53 +0530

    1.0

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,8 +1,9 @@
-
-const AnimatedTitle = () => {
+import React from 'react';
+const AnimatedTitle = ({title,containerClass}) => {
   return (
     <div>
-      
+
+      className="mt-5 text-center text-4xl uppercase leading-[0.8"]
     </div>
   )
 }
