6180ed24f1968065cd6c8921adc73787585da0fb
ashishbehera29 <87660082+ashishbehera29@users.noreply.github.com>
2024-12-27 23:20:10 +0530

    GSAP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import { useRef } from "react";
 import { useState } from "react"
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -20,6 +23,8 @@ const Hero = () => {
             setHasClicked(true)
             setCurrentIndex(upcomingVideoIndex);
     }
+
+    useGSAP()
 
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
@@ -55,9 +60,23 @@ const Hero = () => {
               muted
               loop
               className="absolute left-0 top-0 size-full object-cover object-center"
+              onLoadedData={handleVideoLoad}
             />
           </div>
+          <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+              G<b>a</b>ming
+          </h1>
+          <div className="absolute left-0 top-0 z-40 size-full">
+             <div className="mt-24 px-5 sm:px-10">
+             <h1 className="special-font hero-heading text-blue-100">redefi<b>n</b>e</h1>
+             <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Metagame Layer <br /> Unleash the Play Economy</p>
+             <Button id= "watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow/>} containerClass="!bg-yellow-300 flex-center gap-1"/>
+             </div>
+          </div>
       </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
+              G<b>a</b>ming
+          </h1>
     </div>
   )
 }
